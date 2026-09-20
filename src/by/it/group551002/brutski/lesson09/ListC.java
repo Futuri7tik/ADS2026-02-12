b54e31503aa81a52abae32f7f04a642fdbee78ad
futuri7tik <slavabeytrskiy@gmail.com>
2026-09-20 16:17:19 +0300

    task C done

--- src/by/it/group551002/brutski/lesson09/ListC.java
+++ src/by/it/group551002/brutski/lesson09/ListC.java
@@ -9,6 +9,35 @@ public class ListC<E> implements List<E> {
 
     //Создайте аналог списка БЕЗ использования других классов СТАНДАРТНОЙ БИБЛИОТЕКИ
 
+    private int size = 0;
+    private Object[] elems = new Object[15];
+
+    private void realloc_elems(int min_size) {
+        double k = 1.5;
+        if (min_size > elems.length) {
+            int newCapacity = elems.length == 0 ? 15 : (int) (elems.length * k);
+            if (newCapacity < min_size) {
+                newCapacity = min_size;
+            }
+            Object[] newElements = new Object[newCapacity];
+            System.arraycopy(elems, 0, newElements, 0, size);
+            elems = newElements;
+        }
+    }
+
+    private void check_ind(int ind, boolean non_strict) {
+        if (non_strict) {
+            if (ind < 0 || ind > size) {
+                throw new IndexOutOfBoundsException();
+            }
+        }
+        else {
+            if (ind < 0 || ind >= size) {
+                throw new IndexOutOfBoundsException();
+            }
+        }
+    }
+
     /////////////////////////////////////////////////////////////////////////
     /////////////////////////////////////////////////////////////////////////
     //////               Обязательные к реализации методы             ///////
@@ -16,94 +45,180 @@ public class ListC<E> implements List<E> {
     /////////////////////////////////////////////////////////////////////////
     @Override
     public String toString() {
-        return "";
+        StringBuilder str = new StringBuilder();
+        str.append('[');
+        for (int i = 0; i < size; ++i) {
+            if (i != 0) {
+                str.append(", ");
+            }
+            Object e = elems[i];
+            str.append(e == this ? "(this Collection)" : String.valueOf(e));
+        }
+        str.append("]");
+
+        return str.toString();
     }
 
     @Override
     public boolean add(E e) {
-        return false;
+        realloc_elems(size + 1);
+        elems[size++] = e;
+        return true;
     }
 
     @Override
-    public E remove(int index) {
-        return null;
+    public E remove(int ind) {
+        check_ind(ind, false);
+        E removed = (E) elems[ind];
+        System.arraycopy(elems, ind + 1, elems, ind, size - ind - 1);
+        elems[size--] = null;
+        return removed;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public void add(int index, E element) {
-
+        check_ind(index, true);
+        realloc_elems(size + 1);
+        System.arraycopy(elems, index, elems, index + 1, size - index);
+        elems[index] = element;
+        ++size;
     }
 
     @Override
     public boolean remove(Object o) {
+        int ind = indexOf(o);
+        if (ind != -1) {
+            remove(ind);
+            return true;
+        }
         return false;
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        check_ind(index, false);
+        E old_elem = (E) elems[index];
+        elems[index] = element;
+        return old_elem;
     }
 
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
 
     @Override
     public void clear() {
-
+        for (int i = 0; i < size; ++i)
+            elems[i] = null;
+        size = 0;
     }
 
     @Override
     public int indexOf(Object o) {
-        return 0;
+        for (int i = 0; i < size; ++i) {
+            if (elems[i] == null && o == null) {
+                return i;
+            }
+            if (elems[i] != null && elems[i].equals(o))
+                return i;
+        }
+        return -1;
     }
 
     @Override
     public E get(int index) {
-        return null;
+        check_ind(index, false);
+        return (E) elems[index];
     }
 
     @Override
     public boolean contains(Object o) {
-        return false;
+        return indexOf(o) != -1;
     }
 
     @Override
     public int lastIndexOf(Object o) {
-        return 0;
+        for (int i = size - 1; i > -1; --i) {
+            if (elems[i] == null && o == null) {
+                return i;
+            } else if (elems[i] != null && elems[i].equals(o))
+                return i;
+        }
+        return -1;
     }
 
     @Override
     public boolean containsAll(Collection<?> c) {
-        return false;
+        int j = 0;
+        for (Object obj: c) {
+            if (!contains(obj))
+                return false;
+        }
+        return true;
     }
 
     @Override
     public boolean addAll(Collection<? extends E> c) {
-        return false;
+        if (c.isEmpty())
+            return false;
+
+        for (E el: c) {
+            add(el);
+        }
+        return true;
     }
 
     @Override
     public boolean addAll(int index, Collection<? extends E> c) {
-        return false;
+        if (c.isEmpty())
+            return false;
+
+        int last_ind = index;
+        for (E el: c) {
+            add(last_ind++, el);
+        }
+        return true;
     }
 
     @Override
     public boolean removeAll(Collection<?> c) {
-        return false;
+        if (c.isEmpty())
+            return false;
+
+        boolean is_changed = false;
+        int i = 0;
+        while (i < size) {
+            if (c.contains(elems[i])) {
+                remove(elems[i]);
+                is_changed = true;
+            }
+            else
+                ++i;
+        }
+        return is_changed;
     }
 
     @Override
     public boolean retainAll(Collection<?> c) {
-        return false;
+        boolean is_changed = false;
+        int i = 0;
+        while (i < size) {
+            if (!c.contains(elems[i])) {
+                remove(i);
+                is_changed = true;
+            }
+            else
+                ++i;
+        }
+        return is_changed;
     }
 
     /////////////////////////////////////////////////////////////////////////
